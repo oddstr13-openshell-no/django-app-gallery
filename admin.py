8ffd30d897d496710d41d60259e54a8eac714b64
Odd Stråbø <oddstr13@openshell.no>
2024-01-14 18:36:12 +0100

    Update to Django 1.10

--- admin.py
+++ admin.py
@@ -3,7 +3,7 @@ from gallery.models import Image, Photograph, Camera, Lens
 from sorl.thumbnail.admin import AdminImageMixin
 
 class ImageAdmin(AdminImageMixin, admin.ModelAdmin):
-    list_display = ('title', 'published', 'taken', 'photograph', 'camera', 'image', 'tags') # lens
+    list_display = ('title', 'published', 'taken', 'photograph', 'camera', 'image') # lens, 'tags'
     list_filter = ['published', 'taken', 'photograph', 'camera', 'lens', 'tags']
     search_fields = ['title', 'tags', 'photograph', 'camera', 'lens']
     ordering = ['-taken']
--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$',                  views.index),
     url(r'^imagemap.xml$',      views.imagemap),
-    url(r'^image/(?P<id>.+)/$', views.image),
-    url(r'^tag/(?P<tag>.+)/$',  views.by_tag),
+    url(r'^image/(?P<id>.+)/$', views.image, name='gallery-image'),
+    url(r'^tag/(?P<tag>.+)/$',  views.by_tag, name='gallery-by-tag'),
     url(r'^(?P<id>([0-9]|[1-5][0-9]|6[0-3]))$', views.image_old_redirect),
 ]
